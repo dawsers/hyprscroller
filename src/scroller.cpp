79414bcd15b235ce5eb57aa118cb450eceba631e
Constantin Piber <cp.piber@gmail.com>
2024-07-18 14:10:05 +0200

    Fix call to window data update

--- src/scroller.cpp
+++ src/scroller.cpp
@@ -1649,7 +1649,7 @@ void ScrollerLayout::fullscreenRequestForWindow(PHLWINDOW window,
             window->m_vRealPosition = window->m_vLastFloatingPosition;
             window->m_vRealSize = window->m_vLastFloatingSize;
 
-            window->updateSpecialRenderData();
+            window->updateWindowData();
         } else {
             // if it now got fullscreen, make it fullscreen
 

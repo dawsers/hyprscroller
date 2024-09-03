faf0ce24c87d974bea9de5c8c02b49e7ef49aa3b
dawsers <dawser@gmx.com>
2024-09-03 19:43:46 +0200

    Update MONITORID and WORKSPACEID to hyprland-git

--- src/scroller.h
+++ src/scroller.h
@@ -17,7 +17,7 @@ public:
                                        eDirection = DIRECTION_DEFAULT);
     virtual bool isWindowTiled(PHLWINDOW);
     virtual void onWindowRemovedTiling(PHLWINDOW);
-    virtual void recalculateMonitor(const int &monitor_id);
+    virtual void recalculateMonitor(const MONITORID &monitor_id);
     virtual void recalculateWindow(PHLWINDOW);
     virtual void resizeActiveWindow(const Vector2D &delta, eRectCorner corner,
                                     PHLWINDOW pWindow = nullptr);
@@ -37,16 +37,16 @@ public:
     virtual Vector2D predictSizeForNewWindowTiled();
 
     // New Dispatchers
-    void cycle_window_size(int workspace, int step);
-    void move_focus(int workspace, Direction);
-    void move_window(int workspace, Direction);
-    void align_window(int workspace, Direction);
-    void admit_window_left(int workspace);
-    void expel_window_right(int workspace);
-    void reset_height(int workspace);
-    void set_mode(int workspace, Mode);
-    void fit_size(int workspace, FitSize);
-    void toggle_overview(int workspace);
+    void cycle_window_size(WORKSPACEID workspace, int step);
+    void move_focus(WORKSPACEID workspace, Direction);
+    void move_window(WORKSPACEID workspace, Direction);
+    void align_window(WORKSPACEID workspace, Direction);
+    void admit_window_left(WORKSPACEID workspace);
+    void expel_window_right(WORKSPACEID workspace);
+    void reset_height(WORKSPACEID workspace);
+    void set_mode(WORKSPACEID workspace, Mode);
+    void fit_size(WORKSPACEID workspace, FitSize);
+    void toggle_overview(WORKSPACEID workspace);
 
     void marks_add(const std::string &name);
     void marks_delete(const std::string &name);
@@ -54,7 +54,7 @@ public:
     void marks_reset();
 
 private:
-    Row *getRowForWorkspace(int workspace);
+    Row *getRowForWorkspace(WORKSPACEID workspace);
     Row *getRowForWindow(PHLWINDOW window);
 
     List<Row *> rows;
